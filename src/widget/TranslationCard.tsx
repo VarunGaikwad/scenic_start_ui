ecb560dbdbf076dd2cb07375e85dbb70fe39aced
Varun <varun_gaikwad@jp.honda>
2026-02-18 09:54:44 +0900

    minor URL change for extension

--- src/widget/TranslationCard.tsx
+++ src/widget/TranslationCard.tsx
@@ -1,7 +1,7 @@
 export default function TranslationCard() {
   return (
     <iframe
-      src="https://omni-vox-ten.vercel.app"
+      src="https://omni-vox-ten.vercel.app/?sl=ja&tl=hinglish&text="
       className="w-full h-[75vh] border-none rounded-2xl"
       title="Translation"
     />
